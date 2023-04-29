8965fc6ff7eb4c865b7b97efa2b16afd0e9c92ff
Valentinas Kovalenkinas <v.kovalenkinas@icloud.com>
2023-04-29 12:09:14 +0300

    add some fails

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Valentinas Kovalenkinas on 2023-04-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
