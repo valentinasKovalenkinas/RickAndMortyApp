a780dff1ae30edfa79148aa0f2e233d8169e4048
Valentinas Kovalenkinas <v.kovalenkinas@icloud.com>
2023-05-03 19:17:59 +0300

    no message

--- RickAndMorty/Controllers/Core/RMCharacterTableViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterTableViewController.swift
@@ -15,5 +15,15 @@ final class RMCharacterTableViewController: UITableViewController {
         
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        RMService.shared.exebute(.listCharactersRequest, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print("Total: "+String(model.info.count))
+                print("Page result count: "+String(model.results.count))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
     }
 }
