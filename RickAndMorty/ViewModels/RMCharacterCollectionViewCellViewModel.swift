528ce061c1ce2b6fffdd965ce0824b4b1c9b3b71
Valentinas Kovalenkinas <v.kovalenkinas@icloud.com>
2023-05-11 21:49:37 +0300

    Scroll and load more characters

--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-final class RMCharacterCollectionViewCellViewModel {
+final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
     public let characterName: String
     private let characterStatus: RMCharacterStatus
     private let characterImageUrl: URL?
@@ -44,5 +44,17 @@ final class RMCharacterCollectionViewCellViewModel {
             completion(.success(data))
         }
         task.resume()
+    }
+    
+    // MARK: - Hashable
+    
+    static func == (lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(characterName)
+        hasher.combine(characterStatus)
+        hasher.combine(characterImageUrl)
     }
 }
