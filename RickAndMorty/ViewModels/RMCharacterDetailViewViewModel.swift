049f98dbea6fedba1088758c052a7e1b8cc749d3
Valentinas Kovalenkinas <v.kovalenkinas@icloud.com>
2023-05-14 20:02:15 +0300

    Add sectionLayouts to characterDetail

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -10,8 +10,22 @@ import Foundation
 final class RMCharacterDetailViewViewModel {
     private let character: RMCharacter
     
+    enum SectionType: CaseIterable {
+        case photo
+        case information
+        case episodes
+    }
+    
+    public let sections = SectionType.allCases
+    
+    // MARK: - Init
+    
     init(character: RMCharacter) {
         self.character = character
+    }
+    
+    private var requestUrl: URL? {
+        return URL(string: character.url)
     }
     
     public var title: String {
