55bb99b65da0215fda9babf90dbd4313d23e3cff
Valentinas Kovalenkinas <v.kovalenkinas@icloud.com>
2023-06-28 21:50:19 +0300

    add RMEpisodeDetailView func

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 /// VC to show details about single episode
-final class RMEpisodeDetailViewController: UIViewController {
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
     private let viewModel: RMEpisodeDetailViewViewModel
     
     private let detailView = RMEpisodeDetailView()
@@ -16,7 +16,7 @@ final class RMEpisodeDetailViewController: UIViewController {
     // MARK: - Init
     
     init(url: URL?) {
-        self.viewModel = .init(endpointUrl: url)
+        self.viewModel = RMEpisodeDetailViewViewModel(endpointUrl: url)
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -31,12 +31,13 @@ final class RMEpisodeDetailViewController: UIViewController {
         view.addSubview(detailView)
         addConstraints()
         title = "Episode"
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .action,
             target: self,
             action: #selector(didTapShare)
         )
+        viewModel.delegate = self
+        viewModel.fetchEpisodeData()
     }
     
     private func addConstraints() {
@@ -51,5 +52,11 @@ final class RMEpisodeDetailViewController: UIViewController {
     @objc
     private func didTapShare() {
         
+    }
+    
+    // MARK: - Delegate
+    
+    func didFetchEpisodeDetails() {
+        detailView.configure(with: viewModel)
     }
 }
--- RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -7,28 +7,77 @@
 
 import UIKit
 
-class RMEpisodeDetailViewViewModel {
+protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
+    func didFetchEpisodeDetails()
+}
+
+final class RMEpisodeDetailViewViewModel {
     private let endpointUrl: URL?
+    private var dataTuple: (RMEpisode, [RMCharacter])? {
+        didSet {
+            delegate?.didFetchEpisodeDetails()
+        }
+    }
+    
+    public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
+    
+    // MARK: - Init
     
     init(endpointUrl: URL?) {
         self.endpointUrl = endpointUrl
-        fetchEpisodeData()
     }
     
-    private func fetchEpisodeData() {
+    // MARK: - Public
+    
+    // MARK: - Private
+    
+    /// Fetch backing episode model
+    public func fetchEpisodeData() {
         guard let url = endpointUrl,
               let request = RMRequest(url: url) else {
             return
         }
         
         RMService.shared.exebute(request,
-                                 expecting: RMEpisode.self) { result in
+                                 expecting: RMEpisode.self) { [weak self] result in
             switch result {
-            case .success(let success):
-                print(String(describing: success))
+            case .success(let model):
+                self?.fetchRelatedCharacters(episode: model)
             case .failure:
                 break
             }
+        }
+    }
+    
+    private func fetchRelatedCharacters(episode: RMEpisode) {
+        let requests: [RMRequest] = episode.characters.compactMap({
+            return URL(string: $0)
+        }).compactMap({
+            return RMRequest(url: $0)
+        })
+        
+        let group = DispatchGroup()
+        var characters: [RMCharacter] = []
+        for request in requests {
+            group.enter()
+            RMService.shared.exebute(request, expecting: RMCharacter.self) { result in
+                defer {
+                    group.leave()
+                }
+                switch result {
+                case .success(let model):
+                    characters.append(model)
+                case .failure:
+                    break
+                }
+            }
+        }
+        
+        group.notify(queue: .main) {
+            self.dataTuple = (
+                episode,
+                characters
+            )
         }
     }
 }
